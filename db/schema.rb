ee1b255eff1d859cb3bfe7689a0d2082f02e80fb
Brandon Ruaux <ruaux.brandon@gmail.com>
2020-11-11 16:00:31 +0100

    Create Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_113522) do
+ActiveRecord::Schema.define(version: 2020_11_11_144659) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reviews"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#require 'faker'
+
+#puts 'Creating 5 fake restaurants...'
+#5.times do
+  #restaurant = Restaurant.new(
+    #name:    Faker::Company.name,
+    #address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    #category: [Chinese, Italian, Japanese, French, Belgian]
+  #)
+  #restaurant.save!
+#end
+
+
+restaurants = Restaurant.create([{ name: 'Moko', address: 'Dinan', category: 'japanese' },
+  { name: 'Cosy', address: 'Dinan', category: 'french' },
+  { name: 'La Tomate', address: 'Dinan', category: 'italian' },
+  { name: 'La bonne gaufre', address: 'Dinan', category: 'belgian' },
+  { name: 'Les trois Lune', address: 'Dinan', category: 'french' },
+  { name: 'Le Coquillage', address: 'Saint-Méloir-des-Ondes', category: 'french' }])
